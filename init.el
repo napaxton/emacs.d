3f0f33b26e630374e3803479984d9af186798a76
Nathan A. Paxton <napaxton@gmail.com>
2020-01-15 18:50:15 -0500

    Changes to the jabranham initial
    Changes made to the jabranham gitlab emacs social science kit, using his personal init.el file.

--- init.el
+++ init.el
@@ -40,3 +40,17 @@
 (org-babel-load-file (expand-file-name "init-emacs.org" user-emacs-directory))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ivy-bibtex ivy magit markdown-mode auctex ess use-package exec-path-from-shell company better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block ((t (:inherit default)))))
